34ee351f0b96f3e1aa682c058a8a088dc64f0947
jninfrsh <janinefresha08@gmail.com>
2023-12-09 00:17:38 +0800

    project

---
+++ app/src/main/java/com/basa/janinefresha/block2/p1/quiz/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.basa.janinefresha.block2.p1.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+
+    lateinit var btnAdd : Button
+    lateinit var btnSub : Button
+    lateinit var btnMultiply : Button
+    lateinit var btnDivision : Button
+    lateinit var etA : EditText
+    lateinit var etB : EditText
+    lateinit var resultTv : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnAdd = findViewById(R.id.btn_add)
+        btnSub = findViewById(R.id.btn_subtraction)
+        btnMultiply = findViewById(R.id.btn_multiplication)
+        btnDivision = findViewById(R.id.btn_division
+
+    }
+}
